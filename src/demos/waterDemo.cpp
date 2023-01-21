506a43f42ef8a0d17744ab7bc19670a581ee3118
uk1324 <83022821+uk1324@users.noreply.github.com>
2023-01-21 01:58:39 +0100

    Improved Minecraft-like water

--- src/demos/waterDemo.cpp
+++ src/demos/waterDemo.cpp
@@ -7,12 +7,7 @@
 using namespace ImGui;
 
 WaterDemo::WaterDemo() {
-	for (i64 x = 0; x < GRID_SIZE.x ; x++) {
-		for (i64 y = 0; y < GRID_SIZE.y; y++) {
-			if (x == 0 || x == GRID_SIZE.x - 1 || y == 0 || y == GRID_SIZE.y - 1)
-				cells[x][y].isWall = true;
-		}
-	}
+	reset();
 }
 
 auto WaterDemo::update(Gfx& gfx, Renderer& renderer) -> void {
@@ -30,9 +25,10 @@ auto WaterDemo::update(Gfx& gfx, Renderer& renderer) -> void {
 
 	frameNumber++;
 	if ((!paused && frameNumber % 4 == 0) || singleStep) {
-		for (i64 x = 1; x < GRID_SIZE.x - 1; x++) {
-			for (i64 y = 1; y < GRID_SIZE.y - 1; y++) {
-			/*for (i64 y = GRID_SIZE.y - 2; y > 1; y--) {*/
+
+		for (i64 y = 1; y < GRID_SIZE.y - 1; y++) {
+
+			for (i64 x = 1; x < GRID_SIZE.x - 1; x++) {
 				auto& cell = cells[x][y];
 				if (cell.isWall)
 					continue;
@@ -44,128 +40,38 @@ auto WaterDemo::update(Gfx& gfx, Renderer& renderer) -> void {
 					below.density -= over;
 					cell.density = over;
 				}
-				if (cell.density <= 1)
+			}
+
+			for (i64 x = GRID_SIZE.x - 2; x > 0; x--) {
+				auto& cell = cells[x][y];
+				if (cell.isWall)
+					continue;
+
+				auto& right = cells[x + 1][y];
+				if (!right.isWall && right.density < cell.density) {
+					auto difference = cell.density - right.density;
+					if (difference > 1) {
+						difference /= 2;
+					}
+					cell.density -= difference;
+					right.density += difference;
+				}
+			}
+
+			for (i64 x = 1; x < GRID_SIZE.x - 1; x++) {
+				auto& cell = cells[x][y];
+				if (cell.isWall)
 					continue;
 
 				auto& left = cells[x - 1][y];
-				auto& right = cells[x + 1][y];
-
-				if ((left.density > 0 && cell.density - left.density == 1) && (right.density > 0 && cell.density - right.density == 1))
-					continue;
-
-				if (!right.isWall && right.density < cell.density && !left.isWall && left.density < cell.density) {
-					const auto t = cell.density / 3;
-					const auto l = cell.density % 3;
-					cell.density /= 3;
-					cell.density += l;
-
-
-					left.density += t;
-					const auto overl = std::max(0, left.density - MAX_DENSITY);
-					left.density -= overl;
-					cell.density += overl; 
-					auto o = left.density - cell.density;
-					//if (o > 0) {
-					//	left.density -= o;
-					//	cell.density += o;
-					//}
-
-					right.density += t;
-					const auto overr = std::max(0, right.density - MAX_DENSITY);
-					right.density -= overr;
-					cell.density += overr;
-					//o = right.density - cell.density;
-					//if (o > 0) {
-					//	right.density -= o;
-					//	cell.density += o;
-					//}
-
-				} else if (!right.isWall && right.density < cell.density) {
-					/*if (right.density > 0 && cell.density - right.density == 1)
-						continue;*/
-
-					right.density += cell.density / 2;
-					auto old = cell.density;
-					cell.density /= 2;
-					if (old % 2 == 1) {
-						cell.density += 1;
+				if (!left.isWall && left.density < cell.density) {
+					auto difference = cell.density - left.density;
+					if (difference > 1) {
+						difference /= 2;
 					}
-
-					const auto over = std::max(0, right.density - MAX_DENSITY);
-					right.density -= over;
-					cell.density += over;
-
-					auto o = right.density - cell.density;
-					if (o > 0) {
-						right.density -= o;
-						cell.density += o;
-					}
-				} else if (!left.isWall && left.density < cell.density) {
-					/*if (left.density > 0 && cell.density - left.density == 1)
-						continue;*/
-
-					left.density += cell.density / 2;
-					auto old = cell.density;
-					cell.density /= 2;
-					if (old % 2 == 1) {
-						cell.density += 1;
-					}
-
-					const auto over = std::max(0, left.density - MAX_DENSITY);
-					left.density -= over;
-					cell.density += over;
-
-					auto o = left.density - cell.density;
-					if (o > 0) {
-						left.density -= o;
-						cell.density += o;
-					}
-					
+					cell.density -= difference;
+					left.density += difference;
 				}
-				//auto& below = cells[x][y - 1];
-				//if (!below.isWall) {
-				//	below.density += cell.density;
-				//	const auto over = std::max(0, below.density - MAX_DENSITY);
-				//	below.density -= over;
-				//	cell.density = over;
-				//}
-				//if (cell.density <= 1)
-				//	continue;
-
-				//auto& left = cells[x - 1][y];
-				//auto& right = cells[x + 1][y];
-				//auto l = [&]() {
-				//	if (!left.isWall && left.density < cell.density) {
-				//		left.density += cell.density / 2;
-				//		auto old = cell.density;
-				//		cell.density /= 2;
-				//		if (old % 2 == 1) {
-				//			cell.density += 1;
-				//		}
-
-				//		const auto over = std::max(0, left.density - MAX_DENSITY);
-				//		left.density -= over;
-				//		cell.density += over;
-				//	}
-				//};
-				//auto r = [&]() {
-				//	if (!right.isWall && right.density < cell.density) {
-				//		right.density += cell.density / 2;
-				//		auto old = cell.density;
-				//		cell.density /= 2;
-				//		if (old % 2 == 1) {
-				//			cell.density += 1;
-				//		}
-
-				//		const auto over = std::max(0, right.density - MAX_DENSITY);
-				//		right.density -= over;
-				//		cell.density += over;
-				//	};
-				//};
-
-				//r();
-				//l();
-
 			}
 		}
 	}
@@ -190,19 +96,22 @@ auto WaterDemo::update(Gfx& gfx, Renderer& renderer) -> void {
 		for (i64 y = 1; y < GRID_SIZE.y - 1; y++) {
 			const auto& cell = cells[x][y];
 			ASSERT(cell.density <= MAX_DENSITY);
+			
 			if (cell.isWall) {
 				
 			} else if (cells[x][y].density > 0.0f) {
 				const auto pos = Vec2{ Vec2T{ x, y } } * CELL_SIZE;
-				//const Vec2 verts[] = {
-				//	pos,
-				//	pos + Vec2{ 0, CELL_SIZE },
-				//	pos + Vec2{ CELL_SIZE, CELL_SIZE },
-				//	pos + Vec2{ CELL_SIZE, 0 }
-				//};
-				//Debug::drawSimplePolygon(verts, Vec3{ 14,135,204 } / 255.0f);
 
-				//const auto pos = (Vec2{ Vec2T{ x, y } } + Vec2{ 0.0f, cell.bottomY }) * CELL_SIZE;
+				if (drawGrid) {
+					Vec2 v[] = {
+						pos,
+						pos + Vec2{ CELL_SIZE, 0 },
+						pos + Vec2{ CELL_SIZE, CELL_SIZE },
+						pos + Vec2{ 0, CELL_SIZE }
+					};
+					Debug::drawLines(v, Vec3::WHITE);
+				}
+
 				const auto height = cell.density;
 				std::vector<Vec2> verts;
 				verts.push_back(pos);
@@ -212,72 +121,102 @@ auto WaterDemo::update(Gfx& gfx, Renderer& renderer) -> void {
 					return cell.density / static_cast<float>(MAX_DENSITY) * CELL_SIZE;
 				};
 
+				auto getH = [&](const Vec2T<i64>& cell) -> float {
+					if (cells[cell.x][cell.y + 1].density > 0)
+						return CELL_SIZE;
+					return cells[cell.x][cell.y].density / static_cast<float>(MAX_DENSITY) * CELL_SIZE;
+				};
+
 				auto& right = cells[x + 1][y];
 				auto& left = cells[x - 1][y];
 				auto& belowRight = cells[x + 1][y - 1];
 				auto& belowLeft = cells[x - 1][y - 1];
 				if (cells[x][y + 1].density == 0) {
-					if (!right.isWall && right.density > cell.density && !left.isWall && left.density > cell.density) {
-						verts.push_back(pos + Vec2{ CELL_SIZE, getHeight(right) });
+
+					// Techincally more correct, but without it the water looks smoother. Adds a vertex in the middle if both neighbours are either bigger or smaller.
+										
+					const auto flowingFromRight = !right.isWall && right.density > cell.density;
+					const auto flowingFromLeft = !left.isWall && left.density > cell.density;
+					if (flowingFromLeft && flowingFromRight) {
+						verts.push_back(pos + Vec2{ CELL_SIZE, getH(Vec2T{ x + 1, y }) });
 						verts.push_back(pos + Vec2{ CELL_SIZE / 2.0f, getHeight(cell) });
-						verts.push_back(pos + Vec2{ 0.0f, getHeight(left) });
-					} else if (!left.isWall && left.density > cell.density) {
-						verts.push_back(pos + Vec2{ CELL_SIZE, getHeight(cell) });
-						verts.push_back(pos + Vec2{ 0.0f, getHeight(left) });
-					} else if (!right.isWall && right.density > cell.density) {
-						verts.push_back(pos + Vec2{ CELL_SIZE, getHeight(right) });
-						verts.push_back(pos + Vec2{ 0.0f, getHeight(cell) });
-					} else if (left.density == 0 && right.density == 0) {
-						verts.push_back(pos + Vec2{ CELL_SIZE / 2.0f, getHeight(cell) });
-					} else if (left.density == 0) {
-						if (!right.isWall && right.density > cell.density) {
-							verts.push_back(pos + Vec2{ CELL_SIZE, getHeight(right) });
-						} else {
-							verts.push_back(pos + Vec2{ CELL_SIZE, getHeight(cell) });
-						}
-					} else if (right.density == 0) {
-						if (!left.isWall && right.density > cell.density) {
-							verts.push_back(pos + Vec2{ 0.0f, getHeight(left) });
-						} else {
-							verts.push_back(pos + Vec2{ 0.0f, getHeight(cell) });
-						}
+						verts.push_back(pos + Vec2{ 0.0f, getH(Vec2T{ x - 1, y }) });
+					} else if (!left.isWall && left.density == 0 && !right.isWall && right.density == 0) {
+						// Dividing y by 2 just for the visual effect.
+						verts.push_back(pos + Vec2{ CELL_SIZE / 2.0f, getHeight(cell) / 2.0f });
 					} else {
-						verts.push_back(pos + Vec2{ CELL_SIZE, getHeight(cell) });
-						verts.push_back(pos + Vec2{ 0.0f, getHeight(cell) });
+						if (!right.isWall && right.density == 0) {
+
+						} else if (right.isWall || right.density < cell.density) {
+							verts.push_back(pos + Vec2{ CELL_SIZE, getHeight(cell) });
+						} else {
+							verts.push_back(pos + Vec2{ CELL_SIZE, getH(Vec2T{ x + 1, y }) });
+						}
+
+						if (!left.isWall && left.density == 0) {
+
+						} else if (left.isWall || left.density < cell.density) {
+							verts.push_back(pos + Vec2{ 0.0f, getHeight(cell) });
+						} else  {
+							verts.push_back(pos + Vec2{ 0.0f, getH(Vec2T{ x - 1, y }) });
+						}
 					}
 				} else {
 					verts.push_back(pos + Vec2{ CELL_SIZE, CELL_SIZE });
 					verts.push_back(pos + Vec2{ 0, CELL_SIZE });
 				}
 
-				/*Debug::drawSimplePolygon(verts, Vec3{ 14,135,204 } / 255.0f);*/
-				/*auto p = PixelRgba::scientificColoring(cell.density, 0, 8);
-				Debug::drawSimplePolygon(verts, Vec3(p.r, p.g, p.b) / 255.0f);*/
-				Debug::drawSimplePolygon(verts, Vec3{ 14, 135, 204 } / 255.0f);
+				if (showDensity) {
+					const auto p = PixelRgba::scientificColoring(cell.density, 0, MAX_DENSITY);
+					Debug::drawSimplePolygon(verts, Vec3(p.r, p.g, p.b) / 255.0f);
+				} else {
+					Debug::drawSimplePolygon(verts, Vec3{ 14, 135, 204 } / 255.0f);
+				}
+				
 			}
 		}
 	}
 
-	Debug::drawPoint(Vec2{ 0.0f });
-
 	const auto cursorPos = Vec2T<i64>{ (camera.cursorPos() / CELL_SIZE).applied(floor) };
-	if (Input::isMouseButtonHeld (MouseButton::LEFT)) {
-		cells[cursorPos.x][cursorPos.y].density = MAX_DENSITY;
+	if (cursorPos.x > 0 && cursorPos.y > 0 && cursorPos.x < GRID_SIZE.x - 1 && cursorPos.y < GRID_SIZE.y - 1) {
+		auto& cell = cells[cursorPos.x][cursorPos.y];
+		if (Input::isMouseButtonHeld(MouseButton::LEFT)) {
+			cell.density = MAX_DENSITY;
+		} else if (Input::isMouseButtonHeld(MouseButton::RIGHT)) {
+			cell.isWall = true;
+		} else if (Input::isMouseButtonHeld(MouseButton::MIDDLE)) {
+			cell.isWall = false;
+			cell.density = 0;
+		} else if (Input::isKeyDown(Keycode::A)) {
+			selected = cursorPos;
+		}
 	}
 
-	if (Input::isMouseButtonHeld(MouseButton::RIGHT)) {
-		cells[cursorPos.x][cursorPos.y].isWall = true;
-	}
-
-	if (Input::isMouseButtonHeld(MouseButton::MIDDLE)) {
-		cells[cursorPos.x][cursorPos.y].isWall = false;
-	}
-
+	Begin("water");
 	Checkbox("paused", &paused);
 	singleStep = false;
 	if (paused) {
 		singleStep = Button("single step");
 	}
+	InputInt("density", &cells[selected.x][selected.y].density);
+	if (Button("reset")) {
+		reset();
+	}
+	Checkbox("drawGrid", &drawGrid);
+	Checkbox("showDensity", &showDensity);
+	End();
 
 	renderer.update(gfx, camera);
+}
+
+auto WaterDemo::reset() -> void {
+	for (i64 x = 0; x < GRID_SIZE.x; x++) {
+		for (i64 y = 0; y < GRID_SIZE.y; y++) {
+			auto& cell = cells[x][y];
+			cell.density = 0;
+			if (x == 0 || x == GRID_SIZE.x - 1 || y == 0 || y == GRID_SIZE.y - 1) {
+				cell.isWall = true;
+			}
+		}
+	}
 }
--- src/demos/waterDemo.hpp
+++ src/demos/waterDemo.hpp
@@ -3,26 +3,32 @@
 #include <gfx/gfx.hpp>
 #include <game/renderer.hpp>
 
+// Compute the vertical pressure.
+// Iterate from top to bottom. if water then Pressure = above pressure + 1 else 0
+// Could base the whole simulation on pressure. The issue is that compute both horizontal and vertial pressure you would need to solve s system of equations. I think, not sure.
 struct WaterDemo {
 	WaterDemo();
 	auto update(Gfx& gfx, Renderer& renderer) -> void;
+	auto reset() -> void;
 
 	struct Cell {
-		Vec2 direction{ 0.0f };
-		float bottomY = 0.0f;
 		i32 density = 0;
 		bool isWall = false;
 	};
-	static constexpr i32 MAX_DENSITY = 8;
+	static constexpr i32 MAX_DENSITY = 16;
 
 	usize frameNumber = 0;
 
-	static constexpr Vec2T<i64> GRID_SIZE{ 40, 40 };
+	static constexpr Vec2T<i64> GRID_SIZE{ 15, 15 };
 	Cell cells[GRID_SIZE.x][GRID_SIZE.y];
+
+	Vec2T<i64> selected{ 0, 0 };
 
 	static constexpr auto CELL_SIZE = 0.05f;
 	static constexpr auto CELL_AREA = CELL_SIZE * CELL_SIZE;
 
 	bool paused = false;
 	bool singleStep = false;
+	bool drawGrid = false;
+	bool showDensity = false;
 };
